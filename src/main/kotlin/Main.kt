bff42f2123f489ac1fc91d332b07d944c459733b
Maxim Zamyatin <mzamyatin@abycus.com>
2022-07-12 11:53:21 +0500

    Reverse sequence

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,6 @@
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println(litres(3.0))
-        println(litres(6.7))
-        println(litres(11.8))
+        println(reverseSeq(5))
     }
 }
---
+++ src/main/kotlin/reverseSeq.kt
@@ -0,0 +1,5 @@
+/**Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]*/
+
+fun reverseSeq(n: Int) = (n downTo 1).toList()
