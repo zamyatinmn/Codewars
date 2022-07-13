aae7436dcae9dfc44824df8eea7d194a52f500fa
Maxim Zamyatin <mzamyatin@abycus.com>
2022-07-13 16:22:05 +0500

    Weight for weight

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
-import fundamental.accum
+import rankup.orderWeight
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println(accum("Hello"))
-        println(accum("AzAzA"))
-        println(accum("sDFvjHdfGb"))
+        println(orderWeight("103 123 4444 99 2000"))
+        println(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"))
     }
 }
