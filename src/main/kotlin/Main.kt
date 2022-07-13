dff49d213dd126d79e898c1ed343f2da04346eb0
Maxim Zamyatin <mzamyatin@abycus.com>
2022-07-13 17:07:47 +0500

    Mexican vawe

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
-import rankup.orderWeight
+import rankup.wave
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println(orderWeight("103 123 4444 99 2000"))
-        println(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"))
+        println(wave("hello maza faka"))
+        println(wave("    s        a       "))
     }
 }
