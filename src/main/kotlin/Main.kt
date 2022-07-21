7d28c8bc24daaa8621178700987f2b7ab75db3e7
Maxim Zamyatin <mzamyatin@abycus.com>
2022-07-21 17:26:54 +0500

    Int to IPv4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,58 +1,15 @@
-import fundamental.outed
+import rankup.longToIP
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println(
-            outed(
-                mapOf(
-                    "tim" to 3,
-                    "jim" to 9,
-                    "randy" to 0,
-                    "sandy" to 7,
-                    "andy" to 6,
-                    "katie" to 4,
-                    "laura" to 9,
-                    "saajid" to 6,
-                    "alex" to 0,
-                    "john" to 9,
-                    "mr" to 3
-                ), "andy"
-            )
-        )
-        println(
-            outed(
-                mapOf(
-                    "tim" to 2,
-                    "jim" to 1,
-                    "randy" to 6,
-                    "sandy" to 6,
-                    "andy" to 9,
-                    "katie" to 7,
-                    "laura" to 4,
-                    "saajid" to 1,
-                    "alex" to 5,
-                    "john" to 6,
-                    "mr" to 8
-                ), "tim"
-            )
-        )
-        println(
-            outed(
-                mapOf(
-                    "tim" to 9,
-                    "jim" to 7,
-                    "randy" to 6,
-                    "sandy" to 5,
-                    "andy" to 9,
-                    "katie" to 1,
-                    "laura" to 5,
-                    "saajid" to 7,
-                    "alex" to 2,
-                    "john" to 6,
-                    "mr" to 3
-                ), "randy"
-            )
-        )
+        println(0b10001000100010001111111100100010 shr 8 and 255)
+//        println(0b11111111000000000000000000000000 and 255 shl 24)
+//        println(0b00000000111111110000000000000000)
+//        println(0b00000000000000001111111100000000)
+//        println(0b00000000000000001111111111111111)
+//        println(255 shl 8)
+        println(longToIP(2149583361u))
+        println(longToIP(32u))
     }
 }
