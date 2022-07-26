62df14419c5b888dd49373770e8219a4805f5f16
Maxim Zamyatin <mzamyatin@abycus.com>
2022-07-26 16:34:17 +0500

    English beggars

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
-import fundamental.fireFight
+import fundamental.beggars
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"))
+        println(beggars(listOf(1, 2, 3, 4, 5), 1))
+        println(beggars(listOf(1, 2, 3, 4, 5), 2))
+        println(beggars(listOf(1, 2, 3, 4, 5), 0))
     }
 }
