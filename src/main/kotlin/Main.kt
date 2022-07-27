590591fe52edff17092649411e1265c8e673659e
Maxim Zamyatin <mzamyatin@abycus.com>
2022-07-27 10:36:10 +0500

    Complementary DNA

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,9 @@
-import fundamental.beggars
+import fundamental.makeComplement
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println(beggars(listOf(1, 2, 3, 4, 5), 1))
-        println(beggars(listOf(1, 2, 3, 4, 5), 2))
-        println(beggars(listOf(1, 2, 3, 4, 5), 0))
+        println(makeComplement("ACTTG"))
+        println(makeComplement("AAATTT"))
     }
 }
