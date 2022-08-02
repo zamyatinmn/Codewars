8dd575883874c90f035d0d1027ab48d031284ae4
Maxim Zamyatin <mzamyatin@abycus.com>
2022-08-02 09:05:33 +0500

    Persistent bugger good solution

--- src/main/kotlin/fundamental/PersistentBugger.kt
+++ src/main/kotlin/fundamental/PersistentBugger.kt
@@ -9,17 +9,6 @@ For example (Input --> Output):
 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
 4 --> 0 (because 4 is already a one-digit number)*/
 
-fun persistence(num: Int) : Int {
-    var a = 0
-    var b = num
-    var result = 0
-    while (b >= 10) {
-        b.toString().forEachIndexed { i, c ->
-            if (i == 0) a = c.digitToInt()
-            else a *= c.digitToInt()
-        }
-        result++
-        b = a
-    }
-    return result
-}
+fun persistence(num: Int) = generateSequence(num) {
+    it.toString().map(Character::getNumericValue).reduce { mult, element -> mult * element }
+}.takeWhile { it > 9 }.count()
