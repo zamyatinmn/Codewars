d1ce34411fe3cfff3a8e49bb1c25a764e84e367f
Maxim Zamyatin <mzamyatin@idscan.net>
2022-08-11 08:58:32 +0500

    Total amount of points

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
-import fundamental.Solution
+import fundamental.points
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println(Solution.nameValue(arrayOf("abc", "abc abc")).contentToString())
-        println(Solution.nameValue(arrayOf("codewars", "abc", "xyz")).contentToString())
+        println(points(listOf("1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3")))
+        println(points(listOf("1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4")))
     }
 }
